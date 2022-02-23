762fb42c1455c27de18a4ce1c21997ff33116d60
Sistla Durga Mahender Rao <sd.mahenderrao@gmail.com>
2022-02-23 14:46:08 +0530

    Add to git

---
+++ Vidywathi-web-app1/Vidywathi-web-app1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Vidywathi_web_app1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
